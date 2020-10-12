059a31eddcabe601e50c6e3a5a762deda34cc6a1
Scott Nicholson <scott.nicholson@live.co.uk>
2020-10-12 20:57:42 +0100

    Updates

---
+++ precache-manifest.114e25c67c77d4c5f9dd00d0f87986b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332ad2ebe837a603f82e080b3fad470c",
+    "url": "/my-task-list/index.html"
+  },
+  {
+    "revision": "073e100586122cf3a4ff",
+    "url": "/my-task-list/static/css/main.df8127c1.chunk.css"
+  },
+  {
+    "revision": "45559b8a765050649f3f",
+    "url": "/my-task-list/static/js/2.6cf2a07f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-task-list/static/js/2.6cf2a07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073e100586122cf3a4ff",
+    "url": "/my-task-list/static/js/main.aa0ba3b0.chunk.js"
+  },
+  {
+    "revision": "0ef3a826aa766db8a457",
+    "url": "/my-task-list/static/js/runtime-main.b9fb7756.js"
+  }
+]);
